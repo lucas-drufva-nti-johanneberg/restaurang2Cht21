e4a9a7ad9c59b89cb7ccd0b7afa56347e4c129e2
lucasdrufva <lucas.drufva@gmail.com>
2021-12-08 16:22:17 +0100

    Added footer

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,10 @@ window.onscroll = function() {
     var body = document.getElementsByTagName('body')[0]
     var logo = document.getElementById('logo');
     var title = document.getElementById('title');
-    if(window.pageYOffset < 200){
+    var boxB = document.getElementById('B');
+    var boxC = document.getElementById('C');
+    console.log(window.pageYOffset)
+    if(window.pageYOffset < 400){
         title.style.transform = `translateY(${window.pageYOffset}px)`
     }
     var subtitle = document.getElementById('subtitle');
@@ -17,5 +20,12 @@ window.onscroll = function() {
     if (window.pageYOffset > 100 && window.pageYOffset < 150){
         logo.style.filter = `blur(${(window.pageYOffset-100)/5}px)`
         logo.style.opacity = `${100-((window.pageYOffset-100)/2)}%`
+    }
+
+    if (window.pageYOffset > 150 && window.pageYOffset < 500) {
+        boxB.style.transform = `translateY(${200 - (((window.pageYOffset - 150) * (200 - 0)) / (500 - 150))}px)`
+        console.log("Transformed: " +(((window.pageYOffset - 150) * (400 - 0)) / (400 - 150)) )
+        console.log("cool:" +  (400 - (((window.pageYOffset - 150) * (400 - 0)) / (400 - 150))))
+        boxC.style.transform = `translateY(${400 - (((window.pageYOffset - 150) * (400 - 0)) / (500 - 150))}px)`
     }
 }
